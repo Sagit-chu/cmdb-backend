4fe2597715258afab9cf1856dbb8ebf862b1d885
sagitchu <601096721@qq.com>
2024-08-08 14:15:48 +0800

    add import func

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -43,9 +43,28 @@ const deleteAsset = async (req, res) => {
     }
 };
 
+const importAssets = async (req, res) => {
+    try {
+        const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const data = xlsx.utils.sheet_to_json(sheet);
+
+        for (const asset of data) {
+            await assetModel.addAsset(asset);
+        }
+
+        res.json({ message: 'Data imported successfully' });
+    } catch (error) {
+        console.error('Error importing data:', error);
+        res.status(500).json({ error: 'Failed to import data' });
+    }
+};
+
 module.exports = {
     getAssets,
     createAsset,
     updateAsset,
+    importAssets,
     deleteAsset
 };
