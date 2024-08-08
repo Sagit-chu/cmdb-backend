18fb9894a81a19da432e6a567a8a46348ce8bb56
sagitchu <601096721@qq.com>
2024-08-08 14:17:25 +0800

    add import

--- routes/assetRoutes.js
+++ routes/assetRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const assetController = require('../controllers/assetController');
+const upload = require('../middleware/upload');
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ router.get('/assets', assetController.getAssets);
 router.post('/assets', assetController.createAsset);
 router.put('/assets/:id', assetController.updateAsset);
 router.delete('/assets/:id', assetController.deleteAsset);
+router.post('/assets/import', upload.single('file'), assetController.importAssets);
 
 module.exports = router;
