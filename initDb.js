3afd7796801ecfbae2da78010747e79870972ffe
sagitchu <601096721@qq.com>
2024-08-08 11:20:33 +0800

    use utf8mb4

--- initDb.js
+++ initDb.js
@@ -5,18 +5,18 @@ const initDb = async () => {
     CREATE TABLE IF NOT EXISTS cmdb_assets (
       id INT AUTO_INCREMENT PRIMARY KEY,
       ip VARCHAR(15) NOT NULL,
-      application_system VARCHAR(255),
-      application_manager VARCHAR(255),
-      overall_manager VARCHAR(255),
+      application_system VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      application_manager VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      overall_manager VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
       is_virtual_machine BOOLEAN,
-      resource_pool VARCHAR(255),
-      data_center VARCHAR(255),
-      rack_location VARCHAR(255),
-      sn_number VARCHAR(255),
+      resource_pool VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      data_center VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      rack_location VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+      sn_number VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
       out_of_band_ip VARCHAR(15),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )
+    ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
   `;
 
     try {
