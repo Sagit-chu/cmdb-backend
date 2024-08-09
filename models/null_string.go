4734147289c0f416dcb2d31dd584e8dc359cbfa2
sagitchu <601096721@qq.com>
2024-08-09 10:43:27 +0800

    fix null string

--- models/null_string.go
+++ models/null_string.go
@@ -10,16 +10,20 @@ type NullString struct {
 	sql.NullString
 }
 
+// UnmarshalJSON 自定义 JSON 解析
 func (ns *NullString) UnmarshalJSON(data []byte) error {
 	var s *string
 	if err := json.Unmarshal(data, &s); err != nil {
 		return err
 	}
 	if s != nil {
+		// 如果字符串非空且有效，将其设置为 Valid
 		ns.Valid = true
 		ns.String = *s
 	} else {
+		// 如果字符串是空或 null，设置为无效
 		ns.Valid = false
+		ns.String = ""
 	}
 	return nil
 }
